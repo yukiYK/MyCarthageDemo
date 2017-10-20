ccf151f2f87e1b1d9688453e6bae53aac24017c8
YKyuki <ssss4444222@163.com>
2017-10-20 16:15:56 +0800

    Initial Commit

---
+++ MyCarthageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCarthageDemo
+//
+//  Created by APiX on 2017/10/20.
+//  Copyright © 2017年 APiX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
